4e5c7b4b1890cf01b7fa51d15fa590790fc74d07
Jacquet L <jacquetleme2021@gmail.com>
2023-01-24 03:57:54 -0500

    updates

--- server/index.py
+++ server/index.py
@@ -5,7 +5,7 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/calculate',  methods=['POST'])
+@app.route('/calculate',  methods=['POST', 'GET'])
 @cross_origin()
 def calculate():
     body = request.get_json()
@@ -45,6 +45,6 @@ def calculate():
     return jsonify(resultado)        
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
